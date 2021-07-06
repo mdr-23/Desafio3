d30499f55bc5266ec1f60ea850487fe76aa7c2e9
Martín del Rosario <delrosariomartin23@gmail.com>
2021-07-06 15:34:59 -0300

    archivo js actualizado

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 for (let i = 1; i <= 4; i++) {
     let nombre = prompt("Nombre completo:");
     let socio = prompt("Nro. de socio:");
-    console.log("Familiar N° "+i+" Nombre: "+nombre+" Socio Nro.: " +socio);
-    alert("Turno reservado para el tour por grupo familiar en el estadio. ¡Te esperamos el próximo domingo a partir de las 10:00 en la puerta principal!")    
+    if (parseInt(socio)){
+        console.log("Familiar N° "+i+" Nombre: "+nombre+" Socio Nro.: " +socio);
+        alert("Turno reservado para el tour por grupo familiar en el estadio. ¡Te esperamos el próximo domingo a partir de las 10:00 en la puerta principal!"); 
+    }else{
+        alert("Error en Nro. de Socio. Actualizá la página y volvé a comenzar con el proceso de carga del grupo familiar.")
+    }
+ 
 }
